6b2cc8defe06d7a5253522a4abb94cd0a1b0e6e2
xulina167 <xuxiaolalalala@qq.com>
2026-03-12 13:19:28 +0800

    feat(ex01): add keyboard input and update README

--- ex01/hello_world.c
+++ ex01/hello_world.c
@@ -1,6 +1,8 @@
-#include<stdio.h>
-main()
-{
-printf("Hello World");
-return 0;
+#include <stdio.h>
+int main() {
+    char name[50];
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Hello %s!\n", name);
+    return 0;
 }
